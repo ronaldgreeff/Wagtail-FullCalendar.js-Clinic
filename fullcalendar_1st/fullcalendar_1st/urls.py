f2e83223b0d4e9e682b27a2ef24da47b10931f23
Ron G <ronald.greeff@gmail.com>
2020-01-20 21:57:03 +0000

    cleaned up urls.py

--- fullcalendar_1st/fullcalendar_1st/urls.py
+++ fullcalendar_1st/fullcalendar_1st/urls.py
@@ -9,7 +9,6 @@ from wagtail.documents import urls as wagtaildocs_urls
 from search import views as search_views
 
 # DRF
-from django.urls import include, path
 from rest_framework import routers
 from scheduler import views
 
@@ -25,28 +24,18 @@ urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    # wagtail's:
-    # ^api/v2/ ^pages/
-    # ^api/v2/ ^images/
-    # ^api/v2/ ^documents/
-    # ^static/(?P<path>.*)$
-    # ^media/(?P<path>.*)$
+    # Wagtail's:
     url(r'^api/v2/', api_router.urls),
-
-    url(r'^django-admin/', admin.site.urls),
 
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
     url(r'^search/$', search_views.search, name='search'),
 
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
+    url(r'^django-admin/', admin.site.urls),
+
+    # Wagtail's def. page serving mechanism - last pattern in list:
     url(r'', include(wagtail_urls)),
 
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    url(r'^pages/', include(wagtail_urls)),
 ]
 
 
