13cca1c8d82214f1b9492840e43503a3626edec8
Ronald Greeff <12639901+ronaldgreeff@users.noreply.github.com>
2020-01-28 09:03:21 +0000

    datetime field on wagtail form & page

--- fullcalendar_1st/scheduler/forms.py
+++ fullcalendar_1st/scheduler/forms.py
@@ -1,13 +1,26 @@
-# for direct usage
+# for django direct usage
+# from django import forms
+# class DateForm(forms.Form):
+#     date = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
+
+# for django widget
 from django import forms
-class DateForm(forms.Form):
-    date = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
+from .widgets import XDSoftDateTimePickerInput
+
+class FlavourSuggestionForm(forms.Form): # DateForm
+    date = forms.DateTimeField(
+        input_formats=['%d/%m/%Y %H:%M'],
+        # widget=XDSoftDateTimePickerInput()
+    )
 
 # from django import forms
-# from .widgets import XDSoftDateTimePickerInput
-
-# class DateForm(forms.Form):
-#     date = forms.DateTimeField(
-#         input_formats=['%d/%m/%Y %H:%M'], 
-#         widget=XDSoftDateTimePickerInput()
-#     )
+# from scheduler.models import IceCreamFlavour
+# class FlavourSuggestionForm(forms.ModelForm):
+#     """
+#     A form for suggesting an ice cream flavour. Here we're using a Django ModelForm, but this could
+#     be as simple or as complex as you like -
+#     see https://docs.djangoproject.com/en/1.9/topics/forms/
+#     """
+#     class Meta:
+#         model = IceCreamFlavour
+#         fields = ['flavour_name', 'your_name', 'datetime']
--- fullcalendar_1st/scheduler/models.py
+++ fullcalendar_1st/scheduler/models.py
@@ -1,3 +1,51 @@
-from django.db import models
+# from django.db import models
+# # Create your models here.
 
-# Create your models here.
+from __future__ import unicode_literals
+
+from django.db import models
+from django.shortcuts import render
+
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+class IceCreamFlavour(models.Model):
+    flavour_name = models.CharField(max_length=255)
+    your_name = models.CharField(max_length=255)
+    datetime = models.DateTimeField(auto_now=True)
+
+
+class FlavourSuggestionPage(Page):
+    intro = RichTextField(blank=True)
+    thankyou_page_title = models.CharField(
+        max_length=255, help_text="Title text to use for the 'thank you' page")
+
+    # Note that there's nothing here for specifying the actual form fields -
+    # those are still defined in forms.py. There's no benefit to making these
+    # editable within the Wagtail admin, since you'd need to make changes to
+    # the code to make them work anyway.
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full"),
+        FieldPanel('thankyou_page_title'),
+    ]
+
+    def serve(self, request):
+        from scheduler.forms import FlavourSuggestionForm
+
+        if request.method == 'POST':
+            form = FlavourSuggestionForm(request.POST)
+            if form.is_valid():
+                flavour = form.save()
+                return render(request, 'scheduler/pickdatetime.html', {
+                    'page': self,
+                    'flavour': flavour,
+                })
+        else:
+            form = FlavourSuggestionForm()
+
+        return render(request, 'scheduler/pickdatetime.html', {
+            'page': self,
+            'form': form,
+        })
