effaab0995d6c3c669c8c49baf237571573d1cec
Ron G <ronald.greeff@gmail.com>
2020-02-17 20:25:56 +0000

    user.models added

--- fullcalendar_1st/scheduler/wagtail_hooks.py
+++ fullcalendar_1st/scheduler/wagtail_hooks.py
@@ -9,25 +9,32 @@ from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 
 from wagtail.contrib.modeladmin.views import IndexView
 
-
 # TODO this code probably doesn't belong in wagtail_hooks anymore - move after working
 
-
-class AppointmentIndexView(IndexView):
+class EnquiryIndexView(IndexView):
     AppointmentFormSet = formset_factory(AppointmentForm, extra=1)
     appointment_formset = AppointmentFormSet(initial=Enquiry.objects.values())
+
+class EnquiryAdmin(ModelAdmin):
+    model = Enquiry
+    list_display = ('enquirer', 'service', 'start', 'end')
+    list_per_page = 10
+    index_view_class = EnquiryIndexView
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# formset factory
+# https://docs.djangoproject.com/en/2.2/topics/forms/formsets/
+# customising wagtail admin view
+# https://docs.wagtail.io/en/v2.7.1/reference/contrib/modeladmin/index.html
+# https://docs.wagtail.io/en/v2.7.1/reference/contrib/modeladmin/primer.html#modeladmin-overriding-views
+# https://docs.wagtail.io/en/v2.7.1/reference/contrib/modeladmin/primer.html#modeladmin-overriding-templates
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 class AppointmentAdmin(ModelAdmin):
     model = Appointment
     list_display = ('service', 'doctor', 'start', 'end')
     list_per_page = 10
-    index_view_class = AppointmentIndexView
 
+
+modeladmin_register(EnquiryAdmin)
 modeladmin_register(AppointmentAdmin)
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# formset factory
-# https://docs.djangoproject.com/en/2.2/topics/forms/formsets/
-# wagtail admin view
-# https://docs.wagtail.io/en/v2.7.1/reference/contrib/modeladmin/index.html
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
